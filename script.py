917b57ad760b2bb7b573beb0334e31499fd5536a
Mahmut Özge Karakaya <mokarakaya@gmail.com>
2017-03-02 22:00:56 +0100

    init

---
+++ script.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import json
+
+fname = "data"
+with open(fname) as f:
+    lineJsonList = json.load(f)
+
+for lineJson in lineJsonList:
+    plt.plot(lineJson["xAxis"], [1, 2, 3, 4], label=lineJson["label"])
+
+plt.legend()
+plt.ylabel('some numbers')
+plt.show()
